bfd1a81ac0570ddc040ac088e2992a9501499483
KyleJosling <kylejosling@hotmail.com>
2023-03-13 21:39:24 -0700

    Added light-based rendering

--- RayTracing/src/Renderer.cpp
+++ RayTracing/src/Renderer.cpp
@@ -1,6 +1,18 @@
 #include "Renderer.h"
 #include "Walnut/Random.h"
 
+namespace Utils{
+    static uint32_t ConvertToRGBA(const glm::vec4& color){
+        uint8_t r = (uint8_t)(color.r * 255.0f);
+        uint8_t g = (uint8_t)(color.g * 255.0f);
+        uint8_t b = (uint8_t)(color.b * 255.0f);
+        uint8_t a = (uint8_t)(color.a * 255.0f);
+        uint32_t result = (a << 24 | b << 16 | g << 8 | r);
+        return result;
+    }
+
+}
+
 void Renderer::OnResize(uint32_t width, uint32_t height){
 
 
@@ -33,7 +45,10 @@ void Renderer::Render(){
             };
             coord = coord * 2.0f - 1.0f; // Map the coordinate from 0->1 to -1->1
 
-            m_ImageData[x + y * m_FinalImage->GetWidth()] = PerPixel(coord);
+            glm::vec4 color = PerPixel(coord);
+            color = glm::clamp(color, glm::vec4(0.0f), glm::vec4(1.0f));
+
+            m_ImageData[x + y * m_FinalImage->GetWidth()] = Utils::ConvertToRGBA(color);
         }
     }
 
@@ -41,15 +56,19 @@ void Renderer::Render(){
 
 }
 
-uint32_t Renderer::PerPixel(glm::vec2 coord){
+glm::vec4 Renderer::PerPixel(glm::vec2 coord){
 
     uint8_t r = coord.x * 255.0f;
     uint8_t g = coord.y * 255.0f;
 
-    glm::vec3 rayOrigin(0.0f, 0.0f, 2.0f);
+    glm::vec3 rayOrigin(0.0f, 0.0f, 1.0f);
     // Currently, we define our direction's z coordinate as -1
     glm::vec3 rayDirection(coord.x, coord.y, -1.0f);
+
     float radius = 0.5f;
+    glm::vec3 sphereOrigin(0.0f, 0.0f, 0.0f);
+    glm::vec3 lightDirection(-1.0f, -1.0f, -1.0f);
+    lightDirection = glm::normalize(lightDirection);
     // rayDirection = glm::normalize(rayDirection);
 
     // (bx^2 + by^2 + bz^2)t^2 + (2(axbx + ayby + azbz))t + (ax^2 + ay^2 + az^2- r^2) = 0
@@ -68,9 +87,28 @@ uint32_t Renderer::PerPixel(glm::vec2 coord){
     // Quadratic formula
     float discriminant = b * b - 4.0f * a * c;
 
-    if (discriminant >= 0.0f)
-        return 0xffff00ff;
+    // If there was a discriminant, we hit the sphere with this ray (that corresponds to this pixel)
+    if (discriminant < 0.0f)
+        return glm::vec4( 0, 0, 0, 1);
 
-    return 0xff000000;
+    // Get the solutions - we only calculate the cloests
+    float closestT = (-b - glm::sqrt(discriminant)) / (2.0f * a);
+
+    glm::vec3 hitPosition = rayOrigin + rayDirection * closestT;
+
+    // Get the normal vector to the sphere at the hit position.
+    // The vector points from the origin to the hit position, since its just a sphere.
+    glm::vec3 normalToSphere = glm::normalize(hitPosition - sphereOrigin);
+
+    // Get the light intensity, based on how much the ray bounces back torwards the camera
+    // We use the negative of the light direction so the dot product gives us the correct
+    // result (the scalar magnitude of the vector going TOWARDS the light source, which is
+    // much more useful to us.)
+    // the dot product gives us the cos(angle between vectors)
+    float light = std::max(glm::dot(normalToSphere, -lightDirection), 0.0f);
+
+    glm::vec3 sphereColor = (normalToSphere * 0.5f + 0.5f) * light;
+    // return glm::vec4(sphereColor, 1.0f);
+    return glm::vec4(1.0f, 0.0f, 0.0f, light);
 
 }
