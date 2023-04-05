8cfb4824f630815e63f5f17bf0d778f326bdc38b
KyleJosling <kylejosling@hotmail.com>
2023-04-05 10:12:55 -0700

    Added some GPU stuff

--- RayTracing/src/Test.cu
+++ RayTracing/src/Test.cu
@@ -2,6 +2,23 @@
 #include <iostream>
 #include <math.h>
 #include <iostream>
+#include <cuda.h>
+#define GLM_FORCE_CUDA
+#include <glm/glm.hpp>
+// #include <cublas_v2.h> TODO? 
+
+
+__device__
+uint32_t another_method(float u, float v){
+
+  uint8_t r = (uint8_t)(u * 255.0f);
+  uint8_t g = (uint8_t)(v * 255.0f);
+  uint8_t b = (uint8_t)(0 * 255.0f);
+  uint8_t a = (uint8_t)(1 * 255.0f);
+  uint32_t result = (a << 24 | b << 16 | g << 8 | r);
+
+  return result;
+}
 
 // Kernel function to add the elements of two arrays
 __global__
@@ -12,21 +29,12 @@ void add(uint32_t * image_data, uint32_t width, uint32_t height)
   int x = blockIdx.x * blockDim.x + threadIdx.x;
   int y = blockIdx.y * blockDim.y + threadIdx.y;
   if (x >= width || y >= height) return;
-  image_data[x + y * width] = 0xFF0000FF;
+  image_data[x + y * width] = another_method((float)x/(float)width, (float)y/(float)height);
 
 }
 
 void add_wrapper(uint32_t * image_data, const uint32_t &width, const uint32_t &height)
 {
-    std::cout << width << std::endl;
-    std::cout << height << std::endl;
-
-    for (int i = 0; i < height; i++){
-        for (int j = 0; j < width; j++){
-            image_data[i * width + j] = 0xFF00FFFF;
-        }
-    }
-
     uint32_t tx = 8;
     uint32_t ty = 8;
     dim3 numBlocks((width + tx - 1)/tx, (height + ty -1) / ty); 
